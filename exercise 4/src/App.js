462ea7a6e819d7b39e3e757f691134e8e17c6d00
ScheWann <92758590+ScheWann@users.noreply.github.com>
2024-02-14 02:23:14 -0600

    exercise 4 update

---
+++ exercise 4/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Parent from "./parent.js";
+import ParentUpdate from "./parent_update.js"
+
+function App() {
+  return (
+    <div className="App">
+    {/* <Parent /> */}
+    <ParentUpdate />
+</div>
+  );
+}
+
+export default App;
---
+++ exercise 4/src/parent_update.js
@@ -0,0 +1,50 @@
+import React, { useState, createContext, useContext } from "react";
+
+const context = createContext();
+
+function ParentUpdate() {
+    const [fName, setfName] = useState("firstName");
+    const [lName, setlName] = useState("LastName");
+    return (
+        <context.Provider value={{ fName, lName }}>
+            <div>This is a Parent component</div>
+            <br />
+            <ChildA />
+        </context.Provider>
+    );
+}
+
+function ChildA() {
+    return (
+        <>
+            This is ChildA Component.
+            <br />
+            <ChildB />
+        </>
+    );
+}
+
+function ChildB() {
+    return (
+        <>
+            This is ChildB Component.
+            <br />
+            <ChildC />
+        </>
+    );
+}
+
+function ChildC() {
+    const { fName, lName } = useContext(context);
+    return (
+        <>
+            This is ChildC component.
+            <br />
+            <h3> Data from Parent component is as follows:</h3>
+            <h4>{fName}</h4>
+            <h4>{lName}</h4>
+        </>
+    );
+}
+
+export default ParentUpdate;
